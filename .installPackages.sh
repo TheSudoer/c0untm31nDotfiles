6edfeb220c68fc6499e108803ab7820c4a48b3c4
C0UNTM31N <antoniomontanazapatachavez@tutanota.com>
2022-08-04 10:02:08 -0500

    Added Spotify as package to install

--- .installPackages.sh
+++ .installPackages.sh
@@ -1,5 +1,7 @@
 echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list  && \
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null && \
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - && \
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list && \
 sudo apt update && sudo apt install nala  && \
 wget https://go.microsoft.com/fwlink/?LinkID=760868 && \
 mv index.html\?LinkID\=760868 vscode.deb && \
@@ -7,5 +9,5 @@ sudo dpkg -i vscode.deb && \
 rm -rf vscode.deb && \
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
 brew install go swift && \
-sudo nala install gobjc gobjc++ gdc && \
+sudo nala install gobjc gobjc++ gdc spotify-client && \
 ln -s .bashrc ~/.bashrc
